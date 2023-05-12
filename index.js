c7788160e02e7669567841fcf80f60d7d0256d8e
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-11 20:18:08 -0400

    not done, have to finish

---
+++ index.js
@@ -0,0 +1,7 @@
+let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
+let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
+let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
+let typeS1Count = 0
+let i = 0
+
+
