a90e780713218264462f69942a7df968c3556c0e
Nathaniel Abramov <abramovnatanel@gmail.com>
2023-05-12 13:47:41 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
+let newInventoryCarModels = ['S1', 'S1', 'S2', 'S1', 'S2', 'S1']
 let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
 let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
 let typeS1Count = 0
+let lorrieCount = 0
 let i = 0
+while(i < newInventoryCarModels.length){
+    if(newInventoryCarModels[i] === 'S1'){
+        typeS1Count++
+    }
+    console.log(newInventoryCarModels[i])
+    i++
+} 
 
-
+for (let i = 0; i < newInventoryCarModels.length; i++) {
+    if(newInventoryCarTypes[i] === 'Lorrie'){
+        lorrieCount++
+    }
+}
+console.log(`Number of S1 Cars is:${typeS1Count}`)
+console.log(`Number of lorries is:${lorrieCount}`)
